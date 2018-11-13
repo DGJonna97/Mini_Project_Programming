26b07b71050e3f73d65653156623a3851c423001
meska44 <lmeska17@student.aau.dk>
2018-11-13 10:36:23 +0100

    New commit in Server file (two more functions)

---
+++ Server/Server.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+  return 0;
+};
+
+int recieveMessage (){
+
+  return 0;
+};
+
+
+int sendMessage(){
+
+  return 0;
+};
