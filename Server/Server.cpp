ac803ad34ca22f29a1bae55bab674a3acf53ab28
meska44 <lmeska17@student.aau.dk>
2018-11-14 10:23:35 +0100

    small change

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -2,18 +2,23 @@
 
 using namespace std;
 
+int recieveMessage();
+int sendMessage();
+
+
 int main(int argc, char const *argv[]) {
 
   return 0;
-};
+}
+
 
 int recieveMessage (){
 
   return 0;
-};
+}
 
 
 int sendMessage(){
 
   return 0;
-};
+}
