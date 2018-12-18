edaa221f753c6e3e6156df0cfb130fe91ecaa7da
Artimesh <nkris17@student.aau.dk>
2018-12-18 14:47:39 +0100

    Server for multiple clients

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,24 +1,92 @@
 #include <iostream>
+#include <WS2tcpip.h>
+
+#pragma comment (lib, "ws2_32.lib")
 
 using namespace std;
 
-int recieveMessage();
-int sendMessage();
+void main() {
+  // Initialize winsock
+  WSADATA wsData;
+  WORD ver = MAKEWORD(2, 2);
 
+  int wsOk = WSAStartup(ver, &wsData);
+  if(wsOk != 0){
+    cerr << "Can't Initialize winsock! Terminating" << endl;
+    return;
+  }
 
-int main(int argc, char const *argv[]) {
+  //Create a socket
+  SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+  if(listening == INVALID_SOCKET){
+    cerr << "Can't create a socket! Terminating" << endl;
+    return;
+  }
 
-  return 0;
-}
+  // Bind ip address and port to a socket
+  sockaddr_in hint;
+  hint.sin_family = AF_INET;
+  hint.sin_port = htons(8080);
+  hint.sin_addr.S_un.S_addr = INADDR_ANY;
 
+  bind(listening, (sockaddr*)&hint, sizeof(hint));
 
-int recieveMessage (){
+  // Tell winsock the socket is for listening
+  listen(listening, SOMAXCONN);
 
-  return 0;
-}
+  // Wait for connection
+  sockaddr_in client;
+  int clientSize = sizeof(client);
 
+  SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+  if(clientSocket == INVALID_SOCKET){
+    cerr << "Invalid client socket! Terminating" << endl;
+    return;
+  }
 
-int sendMessage(){
+  char host[NI_MAXHOST];    //Client's remote name
+  char service[NI_MAXSERV]; //Service (port) the client is connected on
 
-  return 0;
+  ZeroMemory(host, NI_MAXHOST);
+  ZeroMemory(service, NI_MAXSERV);
+
+  if(getnameinfo((sockaddr*)&client), sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0){
+    cout << host << " connected on port " << service << endl;
+  }  else{
+    inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+    cout << host << " connected on port " << ntohos(client.sin_port) << endl;
+  }
+
+  // Close listening socket
+  closesocket(listening);
+
+  //While loop accept and echo message back to client
+  char buf[4096];
+
+  while(true){
+    ZeroMemory(buf, 4096);
+
+    //wait for client to send data
+    int bytesRecieved = recv(clientSocket, buf, 4096, 0)
+    if(bytesRecieved == SOCKET_ERROR){
+      cerr << "Error in recv(). Terminating" << endl;
+      break;
+    }
+
+    if(bytesRecieved == 0){
+      cout << "Client disconnected. " << endl;
+      break;
+    }
+
+    //echo message back to client
+    send(clientSocket, buf, bytesRecieved + 1, 0);
+
+  }
+
+  // Close the socket
+  closesocket(clientSocket);
+
+  // Shutdown winsock
+  WSACleanup();
+
 }
